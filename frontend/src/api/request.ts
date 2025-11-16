5a52cb563022e74ef7a68f05bbecb5515faeca77
Njaso <jason749573@163.com>
2025-11-16 16:19:14 +0800

    frontend add env

--- frontend/src/api/request.ts
+++ frontend/src/api/request.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-// const temporary_BASE_URL = "http://10.22.44.62:8000/api/";
+const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 const request = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
-  // baseURL: temporary_BASE_URL,
+  baseURL,
   timeout: 5000,
 });
 
