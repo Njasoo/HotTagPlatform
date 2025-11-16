066b666594a9218a555029021495cf11e92bec30
Njaso <jason749573@163.com>
2025-11-16 16:30:32 +0800

    test baseURL

--- frontend/src/api/request.ts
+++ frontend/src/api/request.ts
@@ -2,8 +2,10 @@ import axios from "axios";
 
 const baseURL = import.meta.env.VITE_API_BASE_URL;
 
+console.log("baseURL:", baseURL);
+
 const request = axios.create({
-  baseURL,
+  baseURL: "https://hottagplatform.onrender.com/api/",
   timeout: 5000,
 });
 
