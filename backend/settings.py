dfd4462962d22310b4d4e325e515d33dfece9278
Njaso <jason749573@163.com>
2025-11-16 13:06:12 +0800

    change app-name

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["your-app-name.onrender.com", "lcoalhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["rebang-api.onrender.com", "lcoalhost", "127.0.0.1"]
 
 
 # Application definition
