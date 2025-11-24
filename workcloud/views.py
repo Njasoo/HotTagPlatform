4d74b203be23d42e9ab2a80c1206b9037283d530
Njaso <jason749573@163.com>
2025-11-24 14:27:31 +0800

    词云添加cache

--- workcloud/views.py
+++ workcloud/views.py
@@ -2,9 +2,15 @@ from rest_framework.views import APIView
 from .models import WordCloud
 from rest_framework.response import Response
 from rest_framework import status
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 
 
 class WordCloudAPIView(APIView):
+    @method_decorator(cache_page(60 * 60))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
     def get(self, request):
         source = request.query_params.get("source")
         url = WordCloud.objects.get(
